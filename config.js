a7de7cc59ede6b433cebe66f955014b4a3963d70
gbot987 <ghobs987@gmail.com>
2023-06-28 12:36:06 +0100

    config fix for build

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 module.exports = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 }
